820350155aa028098d2edb75e10a5458df1f6ba9
Salauddin Ansari <salauddinansari07457@gmail.com>
2026-07-06 10:28:46 +0530

    add demo file

---
+++ Comman/Demo.py
@@ -0,0 +1,19 @@
+# print("Hello sir ji")
+# name ="salauddin ansari"
+# a=2
+# print("My name : ",name)
+# print("My age is:",a)
+
+# name =input("Enter the name")
+# print("Input name is: ",name)
+
+# Take the Number input in python
+
+number=int(input("Enter the age: "))
+print("your enter age :",number)
+
+#take the two number as input
+number1=int(input("Enter first age "))
+number2=int(input("Enter the second age"))
+print("ag1",number1)
+print("age2",number2)
